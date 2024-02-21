244bc78379cae2d9e95f90cdc4723ed6b84a3dfe
Sainikhil92 <ec92sainikhil@gmail.com>
2024-02-21 16:42:40 +0530

    final

--- src/test/java/TestFiles/Task_1_Runner.java
+++ src/test/java/TestFiles/Task_1_Runner.java
@@ -3,15 +3,16 @@ package TestFiles;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/FeaturesFile",
         glue = "Step_def",
-        plugin = {"pretty","html:target/cucumber-reports"}
+        plugin = {"pretty","json:target/cucumber-json-reports/cucumber.json","html:target/cucumber-reports"}
 
 )
+@Test
 public class Task_1_Runner {
-
 
 }
